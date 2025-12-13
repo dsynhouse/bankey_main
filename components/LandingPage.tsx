f77cc80b661a3315618c5e454e2377a523396d97
Rohan Raghani <rohanraghani@Rohans-MacBook-Pro.local>
2025-12-13 11:49:49 +0530

    chore: remove Rive integration

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -5,7 +5,6 @@ import { ArrowRight, Star, TrendingUp, Wallet, Brain, Target, Crown, BookOpen, C
 import Mascot from './Mascot';
 import DsynLabsLogo from './DsynLabsLogo';
 import { SEO } from './SEO';
-import { RiveAnimation } from './RiveAnimation';
 
 const LandingPage: React.FC = () => {
     const [savingsInput, setSavingsInput] = useState(50);
@@ -235,14 +234,7 @@ const LandingPage: React.FC = () => {
                         {/* Mascot - Popped Out (Placed after card to ensure z-index stacking works) */}
                         <div className="absolute -top-12 -left-4 z-50 pointer-events-none">
                             {/* RIVE DEMO REPLACEMENT */}
-                            <div className="w-32 h-32 pointer-events-auto">
-                                <RiveAnimation
-                                    src="https://cdn.rive.app/animations/vehicles.riv"
-                                    stateMachines="bumpy"
-                                    className="w-full h-full"
-                                />
-                            </div>
-                            {/* <Mascot className="w-28 h-28 drop-shadow-lg" mood="cool" /> */}
+                            <Mascot className="w-28 h-28 drop-shadow-lg" mood="cool" />
 
                             {/* Coin Drop Animation */}
                             {demoAction === 'adding' && (
